ccaeddd1089c54003190d6ba5f5aa1edb998260a
PedroPini <augustopini@gmail.com>
2025-05-01 18:35:14 +0800

    Adding object examples

--- dataStructure/index.js
+++ dataStructure/index.js
@@ -1,3 +1,4 @@
+//Arrays
 const GLOBAL_CHIPS_POSITION = 4;
 let menuArray = ['Bread', 'Cheese', 'Chips', 'Chicken', 'Burger']
 let mixArray = ['Bread', 0, true];
@@ -11,6 +12,57 @@ menuArray.pop();
 menuArray.shift();
 
 menuArray.forEach((item, index) => {
-    console.log(`Index ${index}: ${item}`);
+   // console.log(`Index ${index}: ${item}`);
   });
-console.log(menuArray);
+
+
+//Objects
+let userObject = {
+
+    name: 'Pedro',
+    
+    age: 99,
+    
+    email: 'pedro@equinimcollege.com',
+    
+    favourite_foods: ['Burgers', 'Hot dogs']
+    
+    };
+    //add
+    userObject.phone = '123-456-7890'; 
+    userObject['country'] = 'Australia'; 
+    //set-change
+    userObject.age = 29;
+    userObject['name'] = 'Donnie'; 
+    userObject.favourite_foods[1] = 'Pizza'; 
+
+    //Remove
+    userObject.phone = undefined; 
+    userObject.phone = null | 'null'; 
+
+    //DELETE
+    delete userObject.email;                     // removes the email key
+    delete userObject['favourite_foods'];  
+    console.log(userObject)
+
+    for (let key in userObject) {
+        if (userObject.hasOwnProperty(key)) {
+          console.log(`${key}: ${userObject[key]}`);
+        }
+      }
+
+    console.log(Object.entries(userObject));
+      Object.entries(userObject).forEach(([key, value]) => {
+        console.log(`${key}: ${value}`);
+      });
+
+
+
+
+//Eliza question
+    const stringNull = null;
+    if (!stringNull) {
+       //console.log(true)
+    } else {
+        //console.log(false)
+    }
